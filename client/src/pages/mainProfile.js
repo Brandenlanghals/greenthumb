9091712bdc87fac69f3105e4b0f4471705db8527
JamesWalterHudeck <jameswalterhudeck717@gmail.com>
2020-07-16 13:40:40 -0400

    Created a layout for our main profile page

--- client/src/pages/mainProfile.js
+++ client/src/pages/mainProfile.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
 import Profile from "../components/ProfileUserCard";
 import Bulletin from "../components/ProfileCreateBulletin";
 import CurrentBulletin from "../components/ProfileCurrentBulletin";
@@ -6,12 +7,18 @@ import PastBulletin from "../components/ProfilePastBulletin";
 
 function mainProfile() {
   return (
-    <div>
-      <Profile />
-      <Bulletin />
-      <CurrentBulletin />
-      <PastBulletin />
-    </div>
+    <Grid container spacing={3}>
+      <Grid item xs>
+        <Profile />
+      </Grid>
+      <Grid item xs>
+        <Bulletin />
+        <CurrentBulletin />
+      </Grid>
+      <Grid item xs>
+        <PastBulletin />
+      </Grid>
+    </Grid>
   );
 }
 
