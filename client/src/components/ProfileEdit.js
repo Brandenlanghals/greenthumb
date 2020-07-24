f7528559928af43dbc876d00163ef5752934bd0d
Branden Langhals <BrandenLanghals@gmail.com>
2020-07-23 21:30:34 -0400

    landingpage edited

--- client/src/components/ProfileEdit.js
+++ client/src/components/ProfileEdit.js
@@ -1,94 +1,121 @@
-import React, { useState } from "react";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import Typography from "@material-ui/core/Typography";
-import Avatar from "@material-ui/core/Avatar";
-import { makeStyles } from "@material-ui/core/styles";
-import Api from "../utils/api";
+// import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+// import Grid from '@material-ui/core/Grid';
+// import Container from '@material-ui/core/Container';
+// import GitHubIcon from '@material-ui/icons/GitHub';
+// import FacebookIcon from '@material-ui/icons/Facebook';
+// import TwitterIcon from '@material-ui/icons/Twitter';
+// import Header from './Header';
+// import MainFeaturedPost from './MainFeaturedPost';
+// import FeaturedPost from './FeaturedPost';
+// import Main from './Main';
+// import Sidebar from './Sidebar';
+// import Footer from './Footer';
+// import post1 from './blog-post.1.md';
+// import post2 from './blog-post.2.md';
+// import post3 from './blog-post.3.md';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    "& > *": {
-      margin: theme.spacing(1),
-      width: "25ch",
-    },
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   mainGrid: {
+//     marginTop: theme.spacing(3),
+//   },
+// }));
 
-export default function EditProfile() {
-  const classes = useStyles();
+// const sections = [
+//   { title: 'Technology', url: '#' },
+//   { title: 'Design', url: '#' },
+//   { title: 'Culture', url: '#' },
+//   { title: 'Business', url: '#' },
+//   { title: 'Politics', url: '#' },
+//   { title: 'Opinion', url: '#' },
+//   { title: 'Science', url: '#' },
+//   { title: 'Health', url: '#' },
+//   { title: 'Style', url: '#' },
+//   { title: 'Travel', url: '#' },
+// ];
 
-  const handleSubmit = (e) => {
-    Api.editUser({
-      avatar: Image,
-      firstName: firstName,
-      lastName: lastName,
-      about: about,
-      password: password,
-    });
-  };
+// const mainFeaturedPost = {
+//   title: 'Title of a longer featured blog post',
+//   description:
+//     "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+//   image: 'https://source.unsplash.com/random',
+//   imgText: 'main image description',
+//   linkText: 'Continue reading…',
+// };
 
-  const [firstName, setFirstName] = useState();
-  const [lastName, setLastName] = useState();
-  const [about, setAbout] = useState();
-  const [password, setPassword] = useState();
+// const featuredPosts = [
+//   {
+//     title: 'Featured post',
+//     date: 'Nov 12',
+//     description:
+//       'This is a wider card with supporting text below as a natural lead-in to additional content.',
+//     image: 'https://source.unsplash.com/random',
+//     imageText: 'Image Text',
+//   },
+//   {
+//     title: 'Post title',
+//     date: 'Nov 11',
+//     description:
+//       'This is a wider card with supporting text below as a natural lead-in to additional content.',
+//     image: 'https://source.unsplash.com/random',
+//     imageText: 'Image Text',
+//   },
+// ];
 
-  return (
-    <Card className={classes.card}>
-      <form onSubmit={handleSubmit}>
-        <CardContent>
-          <Typography variant="h6" className={classes.title}>
-            Edit Profile
-          </Typography>
-          <br />
-          <TextField
-            id="name"
-            label="firstName"
-            className={classes.textField}
-            margin="normal"
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-          <br />
-          <TextField
-            id="name"
-            label="lastName"
-            className={classes.textField}
-            margin="normal"
-            onChange={(e) => setLastName(e.target.value)}
-          />
-          <br />
-          <TextField
-            id="multiline-flexible"
-            label="about"
-            multiline
-            rows="2"
-            className={classes.textField}
-            margin="normal"
-            onChange={(e) => setAbout(e.target.value)}
-          />
-          <br />
-          <TextField
-            id="password"
-            type="password"
-            label="Change Password"
-            className={classes.textField}
-            margin="normal"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </CardContent>
-        <CardActions>
-          <Button
-            color="primary"
-            variant="contained"
-            className={classes.submit}
-          >
-            Submit
-          </Button>
-        </CardActions>
-      </form>
-    </Card>
-  );
-}
+// const posts = [post1, post2, post3];
+
+// const sidebar = {
+//   title: 'About',
+//   description:
+//     'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.',
+//   archives: [
+//     { title: 'March 2020', url: '#' },
+//     { title: 'February 2020', url: '#' },
+//     { title: 'January 2020', url: '#' },
+//     { title: 'November 1999', url: '#' },
+//     { title: 'October 1999', url: '#' },
+//     { title: 'September 1999', url: '#' },
+//     { title: 'August 1999', url: '#' },
+//     { title: 'July 1999', url: '#' },
+//     { title: 'June 1999', url: '#' },
+//     { title: 'May 1999', url: '#' },
+//     { title: 'April 1999', url: '#' },
+//   ],
+//   social: [
+//     { name: 'GitHub', icon: GitHubIcon },
+//     { name: 'Twitter', icon: TwitterIcon },
+//     { name: 'Facebook', icon: FacebookIcon },
+//   ],
+// };
+
+// export default function Blog() {
+//   const classes = useStyles();
+
+//   return (
+//     <React.Fragment>
+//       <CssBaseline />
+//       <Container maxWidth="lg">
+//         <Header title="Blog" sections={sections} />
+//         <main>
+//           <MainFeaturedPost post={mainFeaturedPost} />
+//           <Grid container spacing={4}>
+//             {featuredPosts.map((post) => (
+//               <FeaturedPost key={post.title} post={post} />
+//             ))}
+//           </Grid>
+//           <Grid container spacing={5} className={classes.mainGrid}>
+//             <Main title="From the firehose" posts={posts} />
+//             <Sidebar
+//               title={sidebar.title}
+//               description={sidebar.description}
+//               archives={sidebar.archives}
+//               social={sidebar.social}
+//             />
+//           </Grid>
+//         </main>
+//       </Container>
+//       <Footer title="Footer" description="Something here to give the footer a purpose!" />
+//     </React.Fragment>
+//   );
+// }
