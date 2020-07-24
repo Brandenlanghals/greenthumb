b5c17c50f1615979d6585c8155e7add00268d5ae
JamesWalterHudeck <jameswalterhudeck717@gmail.com>
2020-07-23 21:33:03 -0400

    created axios call for editUser

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,22 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
-export default{
-    logIn: function(logInData) {
-        return axios.post('/api/user/logIn', logInData)
+export default {
+  logIn: function (logInData) {
+    return axios.post("/api/user/logIn", logInData);
+  },
 
-    },
+  signUp: function (userData) {
+    return axios.post("/api/user/signUp", userData);
+  },
 
-    signUp: function(userData) {
-        return axios.post('/api/user/signUp', userData)
-    },
+  createEvent: function (eventData) {
+    return axios.post("/api/event", eventData);
+  },
 
-    createEvent: function(eventData) {
-        return axios.post('/api/event', eventData)
-    },
+  getUser: function () {
+    return axios.get("/api/user");
+  },
 
-    getUser: function(){
-        return axios.get('/api/user')
-    }
-
-}
-
+  editUser: function (editUser) {
+    return axios.put("/api/user/:id/edit", editUser);
+  },
+};
