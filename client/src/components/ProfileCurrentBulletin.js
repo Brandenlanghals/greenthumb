d867b84449ae0491490b2ebba94f9f646a21e4c5
JamesWalterHudeck <jameswalterhudeck717@gmail.com>
2020-07-30 21:26:32 -0400

    Each event card is dynamically created

--- client/src/components/ProfileCurrentBulletin.js
+++ client/src/components/ProfileCurrentBulletin.js
@@ -1,44 +1,37 @@
 import React, { useState, useEffect } from "react";
-import OutlinedCard from "./card";
 import api from "../utils/api";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+// import CardActions from '@material-ui/core/CardActions';
+import CardContent from "@material-ui/core/CardContent";
+// import Button from '@material-ui/core/Button';
+import Typography from "@material-ui/core/Typography";
 
 function CurrentBulletin() {
-  //const [eventState, setEventState] = useState([]);
+  const [eventState, setEventState] = useState([]);
+
   useEffect(() => {
     const events = [];
     api.getEvent().then((res) => {
-      const events = [];
       console.log(res.data);
-      for (let i = 0; i < 10; i++) {
-        events.push({
-          //eventName: res.data.eventName,
-          //location: res.data.location,
-          date: res.data.date,
-          //startTime: res.data.startTime,
-          //eventDetails: res.data.eventDetails,
-        });
-      }
+      setEventState(res.data);
     });
-    console.log(events);
   }, []);
 
   var style = {
     backgroundColor: "white",
   };
   return (
-    <div>
-      <div style={style}>
-        <OutlinedCard />
-      </div>
-      <div style={style}>
-        <OutlinedCard />
-      </div>
-      <div style={style}>
-        <OutlinedCard />
-      </div>
-      <div style={style}>
-        <OutlinedCard />
-      </div>
+    <div className="">
+      {eventState.map((eachEvent) => (
+        <Card>
+          <CardContent>
+            {eachEvent.eventDate}
+            {eachEvent.eventName}
+            {eachEvent.eventDetails}
+          </CardContent>
+        </Card>
+      ))}
     </div>
   );
 }
