7852f4f01e4c540d57130e99256306c2f339448c
JamesWalterHudeck <jameswalterhudeck717@gmail.com>
2020-07-30 20:12:49 -0400

    Update ProfileCurrentBulletin.js

--- client/src/components/ProfileCurrentBulletin.js
+++ client/src/components/ProfileCurrentBulletin.js
@@ -1,25 +1,45 @@
-import React from "react";
-import OutlinedCard from './card';
+import React, { useState, useEffect } from "react";
+import OutlinedCard from "./card";
+import api from "../utils/api";
 
 function CurrentBulletin() {
- var style ={
-    backgroundColor: 'white'
-    
-  }
+  //const [eventState, setEventState] = useState([]);
+  useEffect(() => {
+    const events = [];
+    api.getEvent().then((res) => {
+      const events = [];
+      console.log(res.data);
+      for (let i = 0; i < 10; i++) {
+        events.push({
+          //eventName: res.data.eventName,
+          //location: res.data.location,
+          date: res.data.date,
+          //startTime: res.data.startTime,
+          //eventDetails: res.data.eventDetails,
+        });
+      }
+    });
+    console.log(events);
+  }, []);
+
+  var style = {
+    backgroundColor: "white",
+  };
   return (
     <div>
-       <div style={style}>
-<OutlinedCard />
-  </div>
-  <div style={style}>
-  <OutlinedCard />
+      <div style={style}>
+        <OutlinedCard />
+      </div>
+      <div style={style}>
+        <OutlinedCard />
+      </div>
+      <div style={style}>
+        <OutlinedCard />
+      </div>
+      <div style={style}>
+        <OutlinedCard />
+      </div>
     </div>
-    <div style={style}>
-    <OutlinedCard />
-      </div><div style={style}>
-<OutlinedCard />
-  </div>
-    </div>
-  )
+  );
 }
 export default CurrentBulletin;
