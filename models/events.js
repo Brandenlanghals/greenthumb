7fda8ddb9bb9efbc5849fc71e18441bf8bc51a84
JamesWalterHudeck <jameswalterhudeck717@gmail.com>
2020-07-31 20:41:34 -0400

    added users to the schema

--- models/events.js
+++ models/events.js
@@ -1,22 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const EventSchema = new Schema ({
-    eventName: {
-        type: String
-    },
-    eventLocation: {
-        type: String
-    },
-    eventDate: {
-        type: Date,
-        default: Date.now
-    },
-    startTime: {
-        type: String
-    },
-    eventDetails: {
-        type: String
-    }
+const EventSchema = new Schema({
+  eventName: {
+    type: String,
+  },
+  eventLocation: {
+    type: String,
+  },
+  eventDate: {
+    type: Date,
+    default: Date.now,
+  },
+  startTime: {
+    type: String,
+  },
+  eventDetails: {
+    type: String,
+  },
+  users: {
+    type: Array,
+  },
 });
-const Event = mongoose.model ('Event', EventSchema);
+const Event = mongoose.model("Event", EventSchema);
 module.exports = Event;
