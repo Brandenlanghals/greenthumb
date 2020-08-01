502843a1ee1a5abd6a88fd29c36358cf773e9cea
Branden Langhals <BrandenLanghals@gmail.com>
2020-07-31 23:12:07 -0400

    added homepage to package.json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const passport = require("./config/passport");
 const routes = require("./routes");
