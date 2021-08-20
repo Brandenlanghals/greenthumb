3dbfb8b4c332cfa4da13d03b83ceaa9098f66ba9
Branden Langhals <BrandenLanghals@gmail.com>
2021-08-20 09:29:42 -0400

    edited about

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -3,7 +3,7 @@ import React from "react";
 function About() {
   return (
     <div>
-      <h1>Green Thumb was created in an effort to bring groups of people together to help make their communities a better place. Whether its planting trees, flowers, or cleaning up trash, we want to see our world grow together.(about)</h1>
+      <h1>Green Thumb was created to bring people together to help make their communities a better place. Whether its planting trees, flowers, or cleaning up trash, we want to see our world grow together.(about)</h1>
     </div>
   );
 }
